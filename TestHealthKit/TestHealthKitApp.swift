0d114cf5011097bb35ad8bd241c41b309b3bc921
Masamichi Ebata <ebata@me.com>
2024-07-22 17:36:49 +0900

    Initial Commit

---
+++ TestHealthKit/TestHealthKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestHealthKitApp.swift
+//  TestHealthKit
+//
+//  Created by Masamichi Ebata on 2024/07/22.
+//
+
+import SwiftUI
+
+@main
+struct TestHealthKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
