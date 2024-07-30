2b45252fa11252e8520adc0f486cc53b5040c365
Masamichi Ebata <ebata@me.com>
2024-07-30 23:18:16 +0900

    Initial

---
+++ TestHealthKit/DataCollection.swift
@@ -0,0 +1,18 @@
+//
+//  DataCollection.swift
+//  TestHealthKit
+//
+//  Created by Masamichi Ebata on 2024/07/25.
+//
+
+import SwiftUI
+
+struct DataCollection: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DataCollection()
+}
---
+++ TestHealthKit/HealthData.swift
@@ -0,0 +1,8 @@
+//
+//  HealthData.swift
+//  TestHealthKit
+//
+//  Created by Masamichi Ebata on 2024/07/29.
+//
+
+import Foundation
