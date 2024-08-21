dee02bcaffd07d0ed72336fa226ba18a38b4fab3
Masamichi Ebata <ebata@me.com>
2024-08-21 16:28:36 +0900

    sheet で step, distnce , 6-minute walk distance を入力可能にした

--- TestHealthKit/HealthDataCollectionView.swift
+++ TestHealthKit/HealthDataCollectionView.swift
@@ -112,20 +112,50 @@ struct HealthDataCollectionView: View {
 }
 
 struct InputCountSheet: View {
-    @State private var date:Date = Date()
-    @State private var inputInt:Int = 0
+    @State private var dateSteps:Date = Date()
+    @State private var valueSteps:Int = 0
+    @State private var dateDistanceWalkingRunning:Date = Date()
+    @State private var valueDistanceWalkingRunning:Double = 0
+    @State private var dateSixMinuteWalkingDistance:Date = Date()
+    @State private var valueSixMinuteWalkingDistance:Int = 0
     @Environment(\.dismiss) private var dismiss
 
     var body: some View {
         NavigationStack {
             List {
-                Section(header: Text("Steps").font(.title)) {
-                    DatePicker ("Date", selection: $date, displayedComponents: .date)
-                    DatePicker ("Time", selection: $date, displayedComponents: .hourAndMinute)
-                    TextField("Step Count", value: $inputInt, format: .number)
+                Section(header: Text("STEPS")) {
+                    DatePicker ("Date", selection: $dateSteps, displayedComponents: .date)
+                    DatePicker ("Time", selection: $dateSteps, displayedComponents: .hourAndMinute)
+                    HStack {
+                        Text("Steps")
+                        TextField("Steps", value: $valueSteps, format: .number)
+                    }
+                    .multilineTextAlignment(.trailing)
+                }
+                
+                Section(header: Text("DISTANCEWALKINGRUNNING")) {
+                    DatePicker ("Date", selection: $dateDistanceWalkingRunning, displayedComponents: .date)
+                    DatePicker ("Time", selection: $dateDistanceWalkingRunning, displayedComponents: .hourAndMinute)
+                    HStack {
+                        Text("Distance")
+                        TextField("Distance", value: $valueDistanceWalkingRunning, format: .number)
+                        .multilineTextAlignment(.trailing)
+                        Text("m")
+                    }
+                }
+                
+                Section(header: Text("SIXMINUTEWALKTESTDISTANCE")) {
+                    DatePicker ("Date", selection: $dateSixMinuteWalkingDistance, displayedComponents: .date)
+                    DatePicker ("Time", selection: $dateSixMinuteWalkingDistance, displayedComponents: .hourAndMinute)
+                    HStack {
+                        Text("Distance")
+                        TextField("Distance", value: $valueSixMinuteWalkingDistance, format: .number)
+                        .multilineTextAlignment(.trailing)
+                        Text("m")
+                    }
                 }
             }
-            .navigationTitle("Steps")
+            .navigationTitle("Manual Settings")
             .navigationBarTitleDisplayMode(.inline)
             .toolbar {
                 ToolbarItem(placement: .topBarLeading) {
